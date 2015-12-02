9822a3650e8a241214d8758ef9de70c14b60445f
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2015-12-02 12:04:02 +0000

    Bump to version `0.1.1`

--- mikla/__init__.py
+++ mikla/__init__.py
@@ -3,7 +3,7 @@ A command line tool to edit text files encrypted with GnuPG whilst
 preventing the plaintext from being written to the hard drive.
 """
 __author__ = 'Matt Deacalion Stevens'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 import getpass
 import hashlib
