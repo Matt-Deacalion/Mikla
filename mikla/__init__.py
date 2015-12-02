cddb8eac2ae3d554b882416188d93cf462477ea4
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2015-12-02 09:33:35 +0000

    Create project skeleton and command line interface

---
+++ mikla/__init__.py
@@ -0,0 +1,10 @@
+"""
+A command line tool to edit text files encrypted with GnuPG whilst
+preventing the plaintext from being written to the hard drive.
+"""
+__author__ = 'Matt Deacalion Stevens'
+__version__ = '0.0.1'
+
+
+class Mikla:
+    pass
