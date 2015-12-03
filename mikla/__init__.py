edda06590820b66955a8b47e153596b253c00b9a
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2015-12-03 10:39:04 +0000

    Add `decrypt` method to `Mikla` class
    This decrypts the encrypted file to a volatile memory based file
    system, in most cases this will be SHM (SHared Memory).

--- mikla/__init__.py
+++ mikla/__init__.py
@@ -8,18 +8,76 @@ __version__ = '0.0.1'
 import getpass
 import os
 import shutil
+import subprocess
 import uuid
 from pathlib import Path
 
 
 class Mikla:
     def __init__(self, **kwargs):
-        self.ciphertext = kwargs.get('<encrypted-file>')
+        self.encrypted_file = kwargs.get('<encrypted-file>')
         self.tmpfs = kwargs.get('--tmpfs')
         self.editor = kwargs.get('--editor')
 
         if self.editor == '$EDITOR':
             self.editor = os.environ['EDITOR']
+
+    def decrypt(self, password, encrypted_file=None, tmpfs=None):
+        """
+        Takes a password string, an encrypted file path and a
+        temporary filesystem path. Then decrypts the encrypted file
+        using GnuPG. Returns the path to a file containing the
+        plaintext.
+        """
+        if encrypted_file is None:
+            encrypted_file = self.encrypted_file
+
+        if tmpfs is None:
+            tmpfs = self.tmpfs
+
+        # do all of the checking manually,
+        # so we don't pointlessly run GnuPG
+        if not self.gpg_exists():
+            raise FileNotFoundError('GnuPG not installed')
+
+        if not os.access(encrypted_file, os.F_OK):
+            raise FileNotFoundError(
+                'File not found: {}'.format(encrypted_file),
+            )
+
+        if not os.access(encrypted_file, os.R_OK):
+            raise FileNotFoundError(
+                'File not readable: {}'.format(encrypted_file),
+            )
+
+        if not os.access(tmpfs, os.W_OK):
+            raise FileNotFoundError(
+                'tmpfs directory not writable: {}'.format(tmpfs),
+            )
+
+        plaintext_path = self.get_available_file_path(tmpfs)
+
+        completed_process = subprocess.run(
+            [
+                'gpg',
+                '--decrypt',
+                '--batch',
+                '--passphrase',
+                password,
+                '--output',
+                plaintext_path,
+                encrypted_file,
+            ],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+        if completed_process.returncode != 0:
+            raise RuntimeError(
+                'Decryption of "{}" failed'.format(encrypted_file),
+            )
+
+        return plaintext_path
 
     def gpg_exists(self, gpg_executable='gpg'):
         """
