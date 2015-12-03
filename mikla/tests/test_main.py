14215bda7338aeba24152f498989647024ebe082
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2015-12-03 11:26:16 +0000

    Add `encrypt` method to `Mikla` class

--- mikla/tests/test_main.py
+++ mikla/tests/test_main.py
@@ -75,3 +75,47 @@ class MiklaTest(unittest.TestCase):
         does not exist?
         """
         self.assertFalse(self.mikla.gpg_exists('the-amber-room'))
+
+    @patch('mikla.subprocess')
+    @patch('mikla.shutil')
+    @patch('mikla.os')
+    def test_encrypt_creates_and_cleans_up_backup(
+        self,
+        mock_os,
+        mock_shutil,
+        mock_subprocess,
+    ):
+        """
+        The `encrypt` method should create a backup copy of the
+        encrypted file and only remove it if the encryption process
+        is successful.
+        """
+        mock_subprocess.run.return_value.returncode = 0
+
+        self.mikla.encrypt('Chunky Hunky', 'plain', 'enc')
+
+        mock_shutil.move.assert_called_once_with('enc', 'enc.bak')
+        mock_os.unlink.assert_called_once_with('enc.bak')
+
+    @patch('mikla.subprocess')
+    @patch('mikla.shutil')
+    @patch('mikla.os')
+    def test_encrypt_creates_and_restores_backup(
+        self,
+        mock_os,
+        mock_shutil,
+        mock_subprocess,
+    ):
+        """
+        The `encrypt` method should create a backup copy of the
+        encrypted file and restore it to it's original name if the
+        encryption process is unsuccessful.
+        """
+        mock_subprocess.run.return_value.returncode = 1
+
+        with self.assertRaises(RuntimeError):
+            self.mikla.encrypt('Chunky Hunky', 'plain', 'enc')
+
+        mock_os.unlink.assert_not_called()
+        mock_shutil.move.assert_called_with('enc.bak', 'enc')
+        self.assertEqual(mock_shutil.move.call_count, 2)
