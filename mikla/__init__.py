be612bb52b255e9247d03f337cd5aeb2cfb3b7ea
Matt Deacalion Stevens <matt@dirtymonkey.co.uk>
2015-12-03 09:42:03 +0000

    Add `get_password` method to `Mikla` class

--- mikla/__init__.py
+++ mikla/__init__.py
@@ -5,6 +5,21 @@ preventing the plaintext from being written to the hard drive.
 __author__ = 'Matt Deacalion Stevens'
 __version__ = '0.0.1'
 
+import getpass
+
 
 class Mikla:
-    pass
+    def __init__(self):
+        pass
+
+    def get_password(self):
+        """
+        Prompts user for a password, returns password string.
+        """
+        while True:
+            password = getpass.getpass('Enter password: ')
+
+            if password == getpass.getpass('Verify password: '):
+                return password
+
+            print("Passwords don't match.")
--- mikla/tests/test_main.py
+++ mikla/tests/test_main.py
@@ -1,7 +1,42 @@
 import unittest
+from contextlib import redirect_stdout
+from io import StringIO
+from unittest.mock import patch
 
 from mikla import Mikla
 
 
 class MiklaTest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.mikla = Mikla()
+
+    @patch('getpass.getpass')
+    def test_get_password_match(self, mock_getpass):
+        """
+        Does the `get_password` method return a password if both
+        entered passwords match?
+        """
+        mock_getpass.side_effect = ['Euclid', 'Euclid']
+
+        self.assertEqual(self.mikla.get_password(), 'Euclid')
+
+    @patch('getpass.getpass')
+    def test_get_password_differ(self, mock_getpass):
+        """
+        Does the `get_password` method notify the user if the entered
+        passwords don't match?
+        """
+        mock_getpass.side_effect = [
+            'Descartes',
+            'Locke',
+            'Wittgenstein',
+            'Wittgenstein',
+        ]
+
+        bucket = StringIO()
+
+        with redirect_stdout(bucket):
+            password = self.mikla.get_password()
+
+        self.assertEqual(password, 'Wittgenstein')
+        self.assertIn("Passwords don't match", bucket.getvalue())
